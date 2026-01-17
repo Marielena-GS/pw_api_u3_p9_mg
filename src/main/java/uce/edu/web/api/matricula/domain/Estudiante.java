5800b2fdf3bcbe9403e19adcb4466c8a8614f79c
aaaaaa <aaaa@gmail.com>
2026-01-16 19:40:30 -0500

    Http_api_quarkus

--- src/main/java/uce/edu/web/api/matricula/domain/Estudiante.java
+++ src/main/java/uce/edu/web/api/matricula/domain/Estudiante.java
@@ -16,8 +16,35 @@ public class Estudiante extends PanacheEntityBase{
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "estudiante_seq")
-    private Integer id;
+    private Long id;
     private String nombre;
     private String apellido;
     private LocalDateTime fechaNacimiento;
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    public LocalDateTime getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+    public void setFechaNacimiento(LocalDateTime fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    
 }
